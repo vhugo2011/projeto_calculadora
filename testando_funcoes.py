dae366faf0bb171d295f91e8fcbe6e6557fb42a9
Victor Hugo <victorhugoalmeida466@gmail.com>
2025-08-30 13:32:32 -0300

    feat: aula de funçoes do tio lucas

---
+++ testando_funcoes.py
@@ -0,0 +1,44 @@
+def estudar():
+    return "Estudando..."
+
+def acordar():
+    return "Estou de pé"
+
+def tomar_cafe():
+    return "Café tomado"
+
+def ir_a_escola():
+    return "Cheguei ao Padre Reus"
+
+def almocar():
+    return "Almocei"
+
+def estudar():
+    return "Estudei python e fiz meus deveres do colégio"
+
+def jogar():
+    return "Amassei no mine dos crias"
+
+def dormir():
+    return "Este cria está a nenecar"
+
+
+def rotina():
+    print(acordar())
+    print(tomar_cafe())
+    print(ir_a_escola())
+    print(almocar())
+    print(estudar())
+    print(jogar())
+    print(dormir())
+
+def subtracao(a,b):
+    return a-b
+
+def main():
+    rotina()
+    print(subtracao(2,3))
+
+if __name__ == "__main__":
+    main()
+
